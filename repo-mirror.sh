5dff1339a3272164ac872c21cf1f3544708eab97
Norgul <marko.dupor@gmail.com>
2019-05-29 14:51:24 +0200

    Paths update

--- repo-mirror.sh
+++ repo-mirror.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-input="repos.txt"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+inputFile="${DIR}/repos.txt"
+mirrorFile="${DIR}/git-mirror.sh"
 
 while IFS= read -r line
 do
 
-    secondary=$(echo ${line} | rev | cut -d' ' -f 1 | rev)
     primary=$(echo ${line} | rev | cut -d' ' -f 2 | rev)
+    secondary=$(echo ${line} | rev | cut -d' ' -f 1 | rev)
 
     echo "Primary repo: $primary"
     echo "Secondary repo: $secondary"
 
-    ./git-mirror.sh ${primary} ${secondary}
+    ${mirrorFile} ${primary} ${secondary}
 
-done < "$input"
+done < "$inputFile"
